40e7395efaaddda1d240821ca4ce1b4c42435075
chenxu10 <shenxu2011@gmail.com>
2025-05-09 19:49:03 -0400

    working

--- tests/test_call_relative_pricing.py
+++ tests/test_call_relative_pricing.py
@@ -1,6 +1,9 @@
 from src.pricing.call_relative import call_price_ratio
 
 def test_call_relative_pricing():
+    """
+    Use SPX 2025-05-09 Price as an example
+    """
     C1_anchor = 63.1
     K1_anchor = 5670
     K = 6795
