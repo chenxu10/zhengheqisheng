a36c5d1bbe37c91e7703820c3f6af549952e20e0
chenxu10 <shenxu2011@gmail.com>
2025-05-08 20:39:03 -0400

    [B]add uv environment and relative call price start to function

---
+++ tests/test_call_relative_pricing.py
@@ -0,0 +1,20 @@
+
+
+def call_price_ratio(K, K_anchor, C_anchor, alpha, spot):
+    return ((K-spot) / (K_anchor-spot)) ** (1 - alpha) * C_anchor
+
+
+def test_call_relative_pricing():
+    C1 = 0.6
+    K1 = 280
+    K2 = 300
+    SPOT = 20
+    alpha_index = 3.2
+    guessed_price_under_power_laws = call_price_ratio(
+        C1, K1, K2, SPOT, alpha_index)
+    market_price = 0.19
+    print(guessed_price_under_power_laws)
+    assert market_price > guessed_price_under_power_laws
+
+if __name__ == "__main__":
+    test_call_relative_pricing()
