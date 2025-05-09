2b0459e0b6f5dff6eb241848c6203b98b5129e7c
chenxu10 <shenxu2011@gmail.com>
2025-05-09 19:47:19 -0400

    [S]separation of concern and move call function

--- tests/test_call_relative_pricing.py
+++ tests/test_call_relative_pricing.py
@@ -1,13 +1,4 @@
-
-
-def call_price_ratio(K, K_anchor, C_anchor, alpha, spot):
-    """
-    Formula implemented from this article
-
-    https://antonismolski.medium.com/tail-options-pricing-under-power-law-with-code-fdb06b1f2567
-    """
-    return ((K-spot) / (K_anchor-spot)) ** (1 - alpha) * C_anchor
-
+from src.pricing.call_relative import call_price_ratio
 
 def test_call_relative_pricing():
     C1_anchor = 63.1
