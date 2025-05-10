f9db814a9c0b43d130397595649e3e89d1c5e27a
chenxu10 <shenxu2011@gmail.com>
2025-05-09 20:22:25 -0400

    working

--- src/pricing/call_relative.py
+++ src/pricing/call_relative.py
@@ -1,7 +1,14 @@
 def call_price_ratio(K, K_anchor, C_anchor, alpha, spot):
     """
-    Formula implemented from this article
+    Caculate realtice call price using power law formulation
+    
+    Reference:https://antonismolski.medium.com/tail-options-pricing-under-power-law-with-code-fdb06b1f2567
 
-    https://antonismolski.medium.com/tail-options-pricing-under-power-law-with-code-fdb06b1f2567
+    Args:
+        K: Target Strike Price,
+        K_anchor: Anchor strike price
+        C_anchor: Anchor call price
+        alpha: Power law exponent
+        spot: Underlying spot price
     """
     return ((K-spot) / (K_anchor-spot)) ** (1 - alpha) * C_anchor
