7fa0f75791457d97ba371267f10513cc9b002b40
chenxu10 <shenxu2011@gmail.com>
2025-05-13 06:25:41 -0400

    working

--- tests/test_put_prices_under_alpha.py
+++ tests/test_put_prices_under_alpha.py
@@ -1,20 +1,36 @@
-def calculate_put_price_under_different_alpha():
+import numpy as np
+from src.pricing.relative_price import put_price_ratio
+
+def calculate_put_price_under_different_alpha(anchorPrice):
+    filteredPuts = {
+        "strike": [160,170,180,190,200,210]
+    }
+    anchorStrike = 0.18
+    strikes = np.arange(min(filteredPuts["strike"]), anchorStrike+1,1)
+    strikes = sorted(strikes, reverse=True)
+    
     put_price_lists_under_alpha = {}
     alpha_values = [3,3.8]
+    prices = [anchorPrice]
     
     for alpha in alpha_values:
-        put_price_lists_under_alpha[alpha] = 0.99
+        prices = [anchorPrice]
+        for i in range(len(strikes)):
+            K_prev = strikes[i - 1]
+            K_curr = strikes[i]
+            C_prev = prices[-1]
+            C_curr = put_price_ratio(K_2=K_curr, K_1=K_prev, K_1_price=C_prev, alpha=alpha)
+            prices.append(C_curr)
 
-    print(put_price_lists_under_alpha)
-
-    
+    put_price_lists_under_alpha[alpha] = prices
     return {
         3: [0.65,0.8],
         3.8:[0.2,0.4]
     }
 
 def test_calculate_put_price_under_different_alpha():
-    result = calculate_put_price_under_different_alpha()
+    anchorPrice = 0.99
+    result = calculate_put_price_under_different_alpha(anchorPrice)
     expected = {
         3: [0.65,0.8],
         3.8:[0.2,0.4]
