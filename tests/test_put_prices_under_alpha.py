d8e4cd87b9eb95acacb355a0907f8513b8bbc107
chenxu10 <shenxu2011@gmail.com>
2025-05-17 10:44:21 -0400

    [S]refactor by asking users to input spot price

--- tests/test_put_prices_under_alpha.py
+++ tests/test_put_prices_under_alpha.py
@@ -1,11 +1,8 @@
 import numpy as np
 from sympy import symbols, Eq, solve
 
-# guess the input of spot is not correct? 
-# solution using equation to solve a spot
-spot = 205.55
 
-def put_price_ratio(to_guess_strikeK, anchorStrikeK, price_at_anchor_strikeK, alpha):
+def put_price_ratio(to_guess_strikeK, anchorStrikeK, price_at_anchor_strikeK, alpha, spot):
     return price_at_anchor_strikeK * ((to_guess_strikeK - spot) / (anchorStrikeK - spot))**(1-alpha)
 
 def create_strike_lists(anchorStrike):
@@ -17,7 +14,7 @@ def create_strike_lists(anchorStrike):
     strikes = [160,170,180,190,200,210]
     return strikes
 
-def calculate_put_price_under_different_alpha(anchorPrice):
+def calculate_put_price_under_different_alpha(anchorPrice, spot):
     anchorStrike = 211
     strikes = create_strike_lists(anchorStrike)
     print("strikes look like", strikes)
@@ -37,7 +34,8 @@ def calculate_put_price_under_different_alpha(anchorPrice):
                 to_guess_strikeK=to_guess_strikeK,
                 anchorStrikeK=anchorStrikeK,
                 price_at_anchor_strikeK=price_at_anchor_strikeK,
-                alpha=3)
+                alpha=3,
+                spot=205.55)
             prices.append(to_guess_model_pricing_at_strikeK)
         
         return strikes
@@ -54,7 +52,10 @@ def calculate_put_price_under_different_alpha(anchorPrice):
 
 def test_calculate_put_price_under_different_alpha():
     anchorPrice = 1.4
-    result = calculate_put_price_under_different_alpha(anchorPrice)
+    spot = 205.55
+    result = calculate_put_price_under_different_alpha(
+        anchorPrice,
+        spot)
     expected = {
         3: [0.65,0.8],
         3.8:[0.2,0.4]
@@ -78,8 +79,10 @@ def test_market_price_smaller_than_theortical_value():
     QQQ strike 415 on Jun/20 Put mark price at 0.40
     """
     anchorPrice = 10.64  #Anchor at 520 05/17' QQQ price
+    spot = 521.51
     theortical_otm_put_value = calculate_put_price_under_different_alpha(
-        anchorPrice
+        anchorPrice,
+        spot
     )
     qqq_415_put_june_20_market = 0.40
     assert qqq_415_put_june_20_market < theortical_otm_put_value
