20b5e1223efc4d580de3774ae007e536ef160d5c
chenxu10 <shenxu2011@gmail.com>
2026-01-02 20:36:34 -0500

    able to do log binning

--- tests/test_plot_alpha.py
+++ tests/test_plot_alpha.py
@@ -17,7 +17,17 @@ def generate_random_samples_from_power_law(alpha, size):
 
 def plot_power_law_hist():
     samples = generate_random_samples_from_power_law(2.5, 1000000)
-    plt.hist(samples)
+
+    # histogram on linear scale
+    plt.subplot(211)
+    hist, bins, _ = plt.hist(samples, bins=100)
+
+    # histogram on log scale. 
+    # Use non-equal bin sizes, such that they look equal on log scale.
+    logbins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
+    plt.subplot(212)
+    plt.hist(samples, bins=logbins)
+    plt.xscale('log')
     plt.show()
 
 def test_generate_random_samples_from_power_law():
