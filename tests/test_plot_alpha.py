e03e214e9e59275a536f02dbc58f10aa3fdbf3ea
chenxu10 <shenxu2011@gmail.com>
2026-01-04 09:42:19 -0500

    [B]transformative method for reproducing is not successful

--- tests/test_plot_alpha.py
+++ tests/test_plot_alpha.py
@@ -1,7 +1,8 @@
 import numpy as np
 import matplotlib.pyplot as plt
 import pytest
-from scipy.stats import pareto, powerlaw
+from scipy.stats import uniform
+from scipy.stats import pareto
 from scipy import stats
 
 def create_emprc_survive_function(data):
@@ -13,26 +14,54 @@ def generate_random_samples_from_power_law(alpha, size):
     b = alpha - 1
     xmin = 1 
     samples = np.clip(pareto.rvs(b, scale=xmin, size=size), 1, 8)
-    #samples = pareto.rvs(b, scale=xmin, size=size)
     return samples
 
 def plot_power_law_hist():
     samples = generate_random_samples_from_power_law(2.5, 1000000)
+    # alpha = -2.5
+    # size = 1000000
+    #samples = genereate_transformative_power_law_samples(alpha, size)
 
     # histogram on linear scale
     plt.subplot(211)
     hist, bins, _ = plt.hist(samples, bins=1000, density=True)
     plt.ylim(0,2)
-    print("bins",bins)
+    plt.xlim(0,8)
+    # plt.show()
 
     # histogram on log scale. 
     # Use non-equal bin sizes, such that they look equal on log scale.
     logbins = np.logspace(np.log10(bins[0]),np.log10(bins[-1]),len(bins))
-    print(logbins)
     plt.subplot(212)
     plt.hist(samples, bins=logbins)
     plt.xscale('log')
     plt.show()
+
+def uniform_sample_r(loc, scale, size):
+    uniformed_sample_r = uniform.rvs(loc=0, scale=1, size=size)
+    return uniformed_sample_r
+
+def genereate_transformative_power_law_samples(alpha, size):   
+    uniform_r = uniform_sample_r(0, 1, size)
+    power_law_samples = [1*(1-r) ** (-1/alpha-1) for r in uniform_r]
+    #samples = [1,2]
+    return power_law_samples
+
+def test_uniform_sample_r():
+    loc = 0
+    scale = 1
+    size = 100000
+    samples = uniform_sample_r(loc, scale, size)
+
+    assert len(samples) == size
+    assert np.min(samples) == pytest.approx(0, abs=1e-4)
+
+def test_genereate_transformative_power_law_samples():
+    alpha = -2.5
+    size = 1000
+    samples = genereate_transformative_power_law_samples(alpha, size)
+
+    assert len(samples) == size
 
 def test_generate_random_samples_from_power_law():
     alpha = 2.5
